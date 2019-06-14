562349c0af70edd7fda0c2f425c31bde3f566cd3
Sarah Harrs <sarahharrs@gmail.com>
2019-06-14 21:24:01 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,8 @@
 def using_while
   levitation_force = 6
-while levitation_force < 6
+while levitation_force < 10
 puts "Wingardium Leviosa"
 levitation_force += 1 
-until levitation_force==10
 end
 
 
